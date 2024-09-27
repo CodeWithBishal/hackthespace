fff7f5d4338ffd56625af5de5361c1f5e6e57d6d
Bishal <69814108+CodeWithBishal@users.noreply.github.com>
2024-09-27 16:39:14 +0530

    Homepage updated and fonts added

--- innovatika/lib/database/informerHardware.dart
+++ innovatika/lib/database/informerHardware.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/material.dart';
 import 'package:realm/realm.dart';
-
 part 'informerHardware.realm.dart';
 
 @RealmModel()
--- innovatika/lib/pages/homepage.dart
+++ innovatika/lib/pages/homepage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:innovatika/database/informer.dart';
+import 'package:innovatika/database/informerHardware.dart';
+import 'package:lottie/lottie.dart';
+import 'package:realm/realm.dart';
 
  class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -10,16 +12,150 @@ import 'package:innovatika/database/informer.dart';
 
 class _HomepageState extends State<Homepage> {
   late bool isOffline = true;
+  Future<List<HardwareInformer>> fetchDevices() async {
+    // Open a Realm instance
+    var config =
+        await Realm.open(Configuration.local(([HardwareInformer.schema])));
+
+    // Fetch all users from MongoDB Realm
+    var users = config.all<HardwareInformer>().toList();
+    return users;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: FutureBuilder(
-        future: uiUpdateHardware(isOffline),
+      body: FutureBuilder<List<HardwareInformer>>(
+        future: fetchDevices(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return snapshot.data ?? Container();
+            var devices = snapshot.data;
+            if (devices!.isEmpty) {
+              return Center(
+                  child: ListView(
+                children: [
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Lottie.asset("assets/animation/empty.json"),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    'No devices found',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ));
+            }
+            return GridView.builder(
+              itemCount: devices.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                childAspectRatio: 1,
+              ),
+              itemBuilder: (context, index) {
+                final device = devices[index];
+                return GestureDetector(
+                  onTap: () {
+                    //TODO
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.purple.shade50,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          offset: const Offset(2, 2),
+                          blurRadius: 5,
+                        ),
+                        BoxShadow(
+                          color: Colors.white.withOpacity(0.5),
+                          offset: const Offset(-2, -2),
+                          blurRadius: 5,
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(15),
+                              child: Image.asset(
+                                'assets/images/device.jpg',
+                                fit: BoxFit.contain,
+                                height: 70,
+                                width: 70,
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                // TODO
+                                // toggleOnOFF(device);
+                              },
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(50),
+                                child: Image.asset(
+                                  isOffline
+                                      ? 'assets/images/off.png'
+                                      : 'assets/images/on.png',
+                                  fit: BoxFit.contain,
+                                  height: 35,
+                                  width: 35,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          color: Colors.white,
+                          width: double.infinity,
+                          child: Text(
+                            isOffline ? 'OFFLINE' : "ONLINE",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontFamily: "Poppins",
+                              fontWeight: FontWeight.bold,
+                              color: isOffline ? Colors.red : Colors.green,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          device.name,
+                          style: const TextStyle(
+                            color: Color(0xff0f52ba),
+                            fontSize: 20,
+                            fontFamily: "Poppins",
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
           } else {
             return Container();
           }
