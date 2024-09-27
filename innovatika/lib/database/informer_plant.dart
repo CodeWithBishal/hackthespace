beed0519d0aec00cf27c248e522684a42fdef684
Bhavesh <bhaveshsaad2006@gmail.com>
2024-09-27 19:34:40 +0530

    made some variables constant

--- innovatika/lib/database/informer_plant.dart
+++ innovatika/lib/database/informer_plant.dart
@@ -6,46 +6,49 @@ part 'informer_plant.realm.dart';
 class _PlantInformer {
   @PrimaryKey()
   @MapTo('_idP')
-  late int? id;
+  late int id;
   late String name;
   late String image;
-  late String? shortDesc;
-  late String? longDesc;
-  late int? timeToGrow;
-  late int? attensionTime;
-  late List<String>? promptHist;
+  late String shortDesc;
+  late String longDesc;
+  late int timeToGrow;
+  late int attensionTime;
+  late List<String> promptHist;
 }
 
 class Plant {
-  late int? id;
+  late int id;
   late String name;
   late String image;
-  late String? shortDesc;
-  late String? longDesc;
+  late String shortDesc;
+  late String longDesc;
   late int timeToGrow;
-  late int? attensionTime;
-  late List<String>? promptHist;
+  late int attensionTime;
+  late List<String> promptHist;
 
   Plant(
-      {this.id,
+      {this.id = 0,
       required this.name,
       required this.image,
-      this.shortDesc,
-      this.longDesc,
+      this.shortDesc = "",
+      this.longDesc = "",
       required this.timeToGrow,
-      this.attensionTime,
+      this.attensionTime = 0,
       this.promptHist = const []});
 
   factory Plant.fromJson(Map<String, dynamic> json) {
     return Plant(
-      id: json['_idP'],
-      name: json['name'],
-      image: json['image'],
-      shortDesc: json['shortDesc'],
-      longDesc: json['longDesc'],
-      timeToGrow: json['timeToGrow'],
-      attensionTime: json['attensionTime'],
-      promptHist: json['promptHist'],
+      id: json['_idP'] ?? 0,
+      name: json['name'] ?? '',
+      image: json['image'] ?? '',
+      shortDesc: json['shortDesc'] ?? '',
+      longDesc: json['longDesc'] ?? '',
+      timeToGrow: json['timeToGrow'] ?? 0,
+      attensionTime: json['attensionTime'] ?? 0,
+      promptHist: (json['promptHist'] as List<dynamic>?)
+              ?.map((e) => e as String)
+              .toList() ??
+          [],
     );
   }
 }
